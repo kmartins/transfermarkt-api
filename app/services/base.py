8ccc4bcb3cbfa801633d2153932b23942bbf053a
Kauê Martins <ksampaiosp@gmail.com>
2026-04-09 16:25:27 -0300

    fix: replace requests with cloudscraper to bypass cloudflare protection

--- app/services/base.py
+++ app/services/base.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass, field
 from typing import Optional
 from xml.etree import ElementTree
 
-import requests
+import cloudscraper
 from bs4 import BeautifulSoup
 from fastapi import HTTPException
 from lxml import etree
@@ -45,26 +45,8 @@ class TransfermarktBase:
         """
         url = self.URL if not url else url
         try:
-            response: Response = requests.get(
-                url=url,
-                headers={
-                    "User-Agent": (
-                        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                        "AppleWebKit/537.36 (KHTML, like Gecko) "
-                        "Chrome/124.0.0.0 "
-                        "Safari/537.36"
-                    ),
-                    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
-                    "Accept-Language": "en-US,en;q=0.9",
-                    "Accept-Encoding": "gzip, deflate, br",
-                    "Cache-Control": "max-age=0",
-                    "Sec-Fetch-Dest": "document",
-                    "Sec-Fetch-Mode": "navigate",
-                    "Sec-Fetch-Site": "none",
-                    "Sec-Fetch-User": "?1",
-                    "Upgrade-Insecure-Requests": "1",
-                },
-            )
+            scraper = cloudscraper.create_scraper()
+            response: Response = scraper.get(url=url)
         except TooManyRedirects:
             raise HTTPException(status_code=404, detail=f"Not found for url: {url}")
         except ConnectionError:
